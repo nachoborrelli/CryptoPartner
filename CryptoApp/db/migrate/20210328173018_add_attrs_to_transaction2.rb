ac550f2348398595162750d658b68f51b3906f16
Nacho Borrelli <nacho2911@hotmail.com>
2021-03-28 16:33:28 -0300

    Home view added (now the app works)

--- CryptoApp/db/migrate/20210328173018_add_attrs_to_transaction2.rb
+++ CryptoApp/db/migrate/20210328173018_add_attrs_to_transaction2.rb
@@ -1,7 +1,7 @@
 class AddAttrsToTransaction2 < ActiveRecord::Migration[6.1]
   def change
-    add_reference :transactions, :coin, null: false ,foreign_key: true
-    add_reference :transactions, :coin, null: false ,foreign_key: true
-    add_reference :transactions, :wallet, null: false ,foreign_key: true
+    # add_reference :transactions, :coin, null: false ,foreign_key: true
+    # add_reference :transactions, :coin, null: false ,foreign_key: true
+    # add_reference :transactions, :wallet, null: false ,foreign_key: true
   end
 end
--- CryptoApp/db/schema.rb
+++ CryptoApp/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_28_004855) do
+ActiveRecord::Schema.define(version: 2021_03_28_173018) do
 
   create_table "coins", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
